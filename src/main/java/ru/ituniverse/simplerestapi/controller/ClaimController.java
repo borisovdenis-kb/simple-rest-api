c92b8ac9065bc86162d7d50451fb96e456ccbbac
borisov <borisovdenis-kb@yandex.ru>
2017-12-20 17:25:06 +0400

    All you need is ~~love~~ @ResponseBody annotation...

--- src/main/java/ru/ituniverse/simplerestapi/controller/ClaimController.java
+++ src/main/java/ru/ituniverse/simplerestapi/controller/ClaimController.java
@@ -2,10 +2,7 @@ package ru.ituniverse.simplerestapi.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -14,11 +11,13 @@ public class ClaimController {
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @RequestMapping(path = "/claims", method = RequestMethod.POST)
+    @ResponseBody
     public String createClaimEcho(@RequestBody String requestBody) {
         return requestBody;
     }
 
     @RequestMapping(path = "/claims", method = RequestMethod.GET)
+    @ResponseBody
     public String getClaim() {
         return "Hello";
     }
